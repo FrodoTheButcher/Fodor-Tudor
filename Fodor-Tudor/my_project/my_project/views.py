3860aefed28155753b032bfb740445778526993e
FrodoTheButcher <122697962+FrodoTheButcher@users.noreply.github.com>
2023-01-30 12:14:11 -0800

    Add files via upload

---
+++ Fodor-Tudor/my_project/my_project/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+def base(request):
+    return render(request,'base.html')
+    
+
+def login(request):
+    return render(request,'login.html')
+
+def test(request):
+    email = request.GET['email']
+    name = request.GET['name']
+    password1 = request.GET['password']
+    password2 = request.GET['password2']
+    return render(request,'test.html',{'email':email
+    ,'name':name,'password1':password1,'password2':password2})
